d4c0ed9e189503112886a154f87fe5c27d603a4d
threatclimber <35009941+threatclimber@users.noreply.github.com>
2018-10-20 18:26:31 +0200

    Added ChordChangeRecord model

--- update_chord_change_record/app.py
+++ update_chord_change_record/app.py
@@ -1,2 +1,16 @@
+from pynamodb.exceptions import UpdateError
+from pynamodb.models import Model
+from pynamodb.attributes import NumberAttribute, UnicodeAttribute
+
+
+class ChordChangeRecord(Model):
+    class Meta:
+        region = 'eu-central-1'
+        table_name = 'chord_change_record'
+
+    chord_change = UnicodeAttribute(hash_key=True)
+    count = NumberAttribute(default=0)
+
+
 def lambda_handler(event, context):
     return True

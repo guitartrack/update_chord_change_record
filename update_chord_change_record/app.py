bc8eba41ecd1f17c161fd5e7ba3be59e5207ecbd
threatclimber <35009941+threatclimber@users.noreply.github.com>
2018-10-20 18:27:23 +0200

    Implemented logic to update chord change records

--- update_chord_change_record/app.py
+++ update_chord_change_record/app.py
@@ -13,4 +13,24 @@ class ChordChangeRecord(Model):
 
 
 def lambda_handler(event, context):
-    return True
+    for record in event['Records']:
+        update_chord_change_record(
+            record['dynamodb']['Keys']['chord_change']['S'],
+            record['dynamodb']['NewImage']['count']['N']
+       )
+
+
+def update_chord_change_record(chord_change: str, count: int):
+    try:
+        record = ChordChangeRecord.get(chord_change)
+    except ChordChangeRecord.DoesNotExist:
+        record = ChordChangeRecord(chord_change=chord_change)
+        record.save()
+
+    try:
+        record.update(
+            actions=[ChordChangeRecord.count.set(count)],
+            condition=(ChordChangeRecord.count < count)
+        )
+    except UpdateError:
+        pass

d9bd58ab5c2c7cc35a3af62a0e396a191dc4d6eb
threatclimber <35009941+threatclimber@users.noreply.github.com>
2018-10-21 14:43:43 +0200

    Add timetracking to records

--- update_chord_change_record/app.py
+++ update_chord_change_record/app.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
 from pynamodb.exceptions import UpdateError
 from pynamodb.models import Model
-from pynamodb.attributes import NumberAttribute, UnicodeAttribute
+from pynamodb.attributes import (
+    NumberAttribute, UnicodeAttribute, UTCDateTimeAttribute)
 
 
 class ChordChangeRecord(Model):
@@ -9,7 +12,11 @@ class ChordChangeRecord(Model):
         table_name = 'chord_change_record'
 
     chord_change = UnicodeAttribute(hash_key=True)
+
     count = NumberAttribute(default=0)
+    first_attempt_at = UTCDateTimeAttribute()
+    last_attempt_at = UTCDateTimeAttribute(null=True)
+    last_record_at = UTCDateTimeAttribute(null=True)
 
 
 def lambda_handler(event, context):
@@ -21,15 +28,23 @@ def lambda_handler(event, context):
 
 
 def update_chord_change_record(chord_change: str, count: int):
+    now = datetime.utcnow()
+
     try:
         record = ChordChangeRecord.get(chord_change)
     except ChordChangeRecord.DoesNotExist:
-        record = ChordChangeRecord(chord_change=chord_change)
+        record = ChordChangeRecord(
+            chord_change=chord_change, first_attempt_at=now)
         record.save()
+
+    record.update(actions=[ChordChangeRecord.last_attempt_at.set(now)])
 
     try:
         record.update(
-            actions=[ChordChangeRecord.count.set(count)],
+            actions=[
+                ChordChangeRecord.count.set(count),
+                ChordChangeRecord.last_record_at.set(now)
+            ],
             condition=(ChordChangeRecord.count < count)
         )
     except UpdateError:
